da68413bb8ebea77caf183ad23c734ff59e14678
Mohsen Azimi <msn.azi@gmail.com>
2014-10-18 17:15:08 -0700

    Add FilesViewController

--- put.io/AppDelegate.swift
+++ put.io/AppDelegate.swift
@@ -15,7 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        var filesViewController = FilesViewController()
+        var navigationController = UINavigationController(rootViewController: filesViewController)
+
+        window?.rootViewController = navigationController
         return true
     }
 
