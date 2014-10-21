93edec31bea55675d791be30b46bd46684988ab8
Mohsen Azimi <msn.azi@gmail.com>
2014-10-21 01:10:25 -0700

    Handle wrong password

--- put.io/LoginViewController.swift
+++ put.io/LoginViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class LoginViewController: UIViewController , UIWebViewDelegate {
     var username:String = ""
     var password:String = ""
+    var triedUsernamePassword:Bool = false
     
     @IBOutlet weak var usernameField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
@@ -20,25 +21,28 @@ class LoginViewController: UIViewController , UIWebViewDelegate {
         login()
     }
     func login(){
-        let url = "https://api.put.io/v2/oauth2/authenticate?client_id=1655" +
-        "&response_type=token&redirect_uri=http://mohsenweb.com/put.io/"
-        var webView = UIWebView()
-        let URL = NSURL(string: url)
-        
         username = usernameField.text
         password = passwordField.text
 
         username = username.stringByTrimmingCharactersInSet(.whitespaceAndNewlineCharacterSet())
         password = password.stringByTrimmingCharactersInSet(.whitespaceAndNewlineCharacterSet())
         if !username.isEmpty && !password.isEmpty {
-            spinner.hidden = false
-            spinner?.startAnimating()
-            webView.delegate = self
-            view.addSubview(webView)
-            webView.loadRequest(NSURLRequest(URL: URL))
-        } else {
-            
+            triedUsernamePassword = false
+            makeRequest()
         }
+    }
+    
+    func makeRequest(){
+        let url = "https://api.put.io/v2/oauth2/authenticate?client_id=1655" +
+        "&response_type=token&redirect_uri=http://mohsenweb.com/put.io/"
+        var webView = UIWebView()
+        let URL = NSURL(string: url)
+        
+        spinner.hidden = false
+        spinner?.startAnimating()
+        webView.delegate = self
+        view.addSubview(webView)
+        webView.loadRequest(NSURLRequest(URL: URL))
     }
     
     func webViewDidFinishLoad(webView: UIWebView) {
@@ -47,20 +51,26 @@ class LoginViewController: UIViewController , UIWebViewDelegate {
         let loginJs = "document.querySelector('[name=\"name\"]').value = '\(username)';" +
             "document.querySelector('[name=\"password\"]').value = '\(password)';" +
         "document.querySelector('[type=\"submit\"]').click();"
+        let range = fragment?.rangeOfString("access_token=")
+        let token = fragment?.substringFromIndex(13) // 13 is "access_token=" length
         
-        if fragment != nil {
-            if let range = fragment?.rangeOfString("access_token=") {
-                if let token = fragment?.substringFromIndex(13) { // 13 is "access_token=" length
-                    UserManager.saveUserToken(token)
-                    tabBarController?.tabBar.hidden = false;
-                    tabBarController?.selectedIndex = 0
-                    navigationController?.setNavigationBarHidden(false, animated: false)
-                    navigationController?.popToRootViewControllerAnimated(false)
-                }
-            }
-        } else {
+        if (fragment != nil) && (range != nil) && (token != nil){
+            FinishLogin(token!)
+        } else if !triedUsernamePassword {
+            triedUsernamePassword = true
             webView.stringByEvaluatingJavaScriptFromString(loginJs)
+        } else {
+            passwordField.text = ""
+            spinner.hidden = true
         }
+    }
+    
+    func FinishLogin(token:String) {
+        UserManager.saveUserToken(token)
+        tabBarController?.tabBar.hidden = false;
+        tabBarController?.selectedIndex = 0
+        navigationController?.setNavigationBarHidden(false, animated: false)
+        navigationController?.popToRootViewControllerAnimated(false)
     }
     
     override func loadView() {
