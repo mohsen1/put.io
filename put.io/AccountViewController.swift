edfc500dd9b9e4f28b1eeae78401b66974077c44
Mohsen Azimi <msn.azi@gmail.com>
2014-10-22 00:28:51 -0700

    Add account details to account page

--- put.io/AccountViewController.swift
+++ put.io/AccountViewController.swift
@@ -7,25 +7,32 @@
 //
 
 import UIKit
+import SwiftHTTP
 
-class AccountViewController: UITableViewController {
+class AccountViewController: UITableViewController, UIAlertViewDelegate {
+    var info = NSDictionary()
     
-    func openLogin(animated:Bool) {
+    func openLogin() {
+        UserManager.deleteUserToken()
         let loginViewController = LoginViewController()
-        navigationController?.pushViewController(loginViewController, animated: animated)
+        navigationController?.pushViewController(loginViewController, animated: true)
     }
     
     // MARK: View
     override func viewWillAppear(animated: Bool) {
         navigationItem.title = "Account"
         self.tableView.registerClass(UITableViewCell.classForCoder(), forCellReuseIdentifier: "UITableViewCell")
+        
+        let logout = UIBarButtonItem(title: "Logout", style: .Plain, target: self, action: "openLogin")
+        navigationItem.rightBarButtonItem = logout
+        self.fetchList()
         super.viewWillAppear(animated)
     }
 
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
         if UserManager.getUserToken() == nil {
-            openLogin(false)
+            openLogin()
         }
     }
 
@@ -34,26 +41,60 @@ class AccountViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    // MARK: - HTTP
+    func fetchList() {
+        let request = HTTPTask()
+        let url = "https://api.put.io/v2/account/info"
+        var params = Dictionary<String, String>()
+        if let token = UserManager.getUserToken() {
+            params = ["oauth_token": "\(token)"]
+        }
+        
+        request.GET(url, parameters: params, success: {(response: HTTPResponse) in
+            if let data = response.responseObject as? NSData {
+                var jsonError:NSError?
+                if let json:NSDictionary = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: &jsonError) as? NSDictionary {
+                    self.info = json["info"] as NSDictionary
+                    dispatch_async(dispatch_get_main_queue()) {
+                        self.tableView.reloadData()
+                    }
+                }
+            }
+            }, failure: {(error: NSError) in
+                var alert = UIAlertView(title: "Network Error", message: "Error fetching account inoformation!", delegate: self, cancelButtonTitle: "OK", otherButtonTitles: "Retry")
+                
+                dispatch_async(dispatch_get_main_queue()) {
+                    alert.show()
+                }
+                
+        })
+    }
+    
+    
     // MARK: - TableView
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return info.count
     }
     
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("UITableViewCell", forIndexPath: indexPath) as UITableViewCell
 
+        let key = info.allKeys[indexPath.row] as? NSString
+        let value = info.allValues[indexPath.row] as? NSString
         
-        cell.textLabel?.text = "Logout"
+        if key != nil && value != nil {
+            cell.textLabel?.text = "\(key!): \(value!)"
+        } else {
+            cell.textLabel?.text = "Unknown"
+        }
         
         return cell
     }
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        if indexPath.row == 0 {
-            UserManager.deleteUserToken()
-            openLogin(true)
-        }
+
 
     }
 
