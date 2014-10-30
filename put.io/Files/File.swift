6e61f5550686ff52a9539e4c30509562d9b69e97
Mohsen Azimi <msn.azi@gmail.com>
2014-10-29 22:06:46 -0700

    Update File model to include all possible values

--- put.io/Files/File.swift
+++ put.io/Files/File.swift
@@ -13,13 +13,21 @@ import CoreData
 class File: NSManagedObject {
     @NSManaged var id: String
     @NSManaged var parent_id: String
-    @NSManaged var name: String
-    @NSManaged var content_type: String
+    @NSManaged var name: String?
+    @NSManaged var content_type: String?
     @NSManaged var isFolder: Bool
-//    @NSManaged var parent: File
-//    @NSManaged var children: NSSet
+    @NSManaged var created_at: NSDate?
+    @NSManaged var first_accessed_at: NSDate?
+    @NSManaged var is_mp4_available: NSNumber? // 0 false, 1 true
+    @NSManaged var is_shared: NSNumber? // 0 fasle, 1 true
+    @NSManaged var opensubtitles_hash: String?
+    @NSManaged var screenshot: String?
+    @NSManaged var size: String?
     
     func fillWithJson(json:NSDictionary){
+        var dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+        
         if let _name = json["name"] as? String {
             name = _name
         }
@@ -32,12 +40,29 @@ class File: NSManagedObject {
         if let _content_type = json["content_type"] as? String {
             content_type = _content_type
         }
-        
-        
-        // Determine if it's a folder and assign children
-        if content_type == "application/x-directory" {
-            isFolder = true
-//            FileStore.getFolder(id, { (files:[File]) in self.children = files})
+        if let _created_at = json["created_at"] as? String {
+            created_at = dateFormatter.dateFromString(_created_at)!
         }
+        if let _first_accessed_at = json["first_accessed_at"] as? String {
+            created_at = dateFormatter.dateFromString(_first_accessed_at)!
+        }
+        if let _is_mp4_available = json["is_mp4_available"] as? Bool {
+            is_mp4_available = NSNumber(bool: _is_mp4_available)
+        }
+        if let _is_shared = json["is_shared"] as? Bool{
+            is_shared = NSNumber(bool: _is_shared)
+        }
+        if let _opensubtitles_hash = json["opensubtitles_hash"] as? String {
+            opensubtitles_hash = _opensubtitles_hash
+        }
+        if let _screenshot = json["screenshot"] as? String {
+            screenshot = _screenshot
+        }
+        if let _size = json["size"] as? String {
+            size = _size
+        }
+
+        // Determine if it's a folder and assign children
+        isFolder = content_type == "application/x-directory"
     }
 }
---
+++ put.io/Files/Types.swift
@@ -0,0 +1,30 @@
+//
+//  Icon.swift
+//  put.io
+//
+//  Created by Mohsen Azimi on 10/29/14.
+//  Copyright (c) 2014 Mohsen Azimi. All rights reserved.
+//
+
+import UIKit
+
+class Types {
+    // TODO: do not use string for types. Use a class variable when available 
+    // class let STRING_TYPES<Int, String> = [ -1: "Plain", 0 : "Video", ...]
+    
+    class func typeFor(contentType: String) -> String? {
+        if let range = contentType.rangeOfString("video/") {
+            return "Video"
+        }
+        if let range = contentType.rangeOfString("image/") {
+            return "Image"
+        }
+        return nil
+    }
+    class func iconFor(contentType: String) -> UIImage {
+        if let type = self.typeFor(contentType) {
+            return UIImage(named: type + "Icon")!
+        }
+        return UIImage(named: "PlainIcon")!
+    }
+}
