fcae935886075f73f962cb3522aebfa362c5b572
Mohsen Azimi <msn.azi@gmail.com>
2014-11-01 13:59:49 -0700

    Add cancel button in transfer

--- put.io/Transfers/TransferViewController.swift
+++ put.io/Transfers/TransferViewController.swift
@@ -7,10 +7,13 @@
 //
 
 import UIKit
+import SwiftHTTP
 
-
-class TransferViewController: UITableViewController {
+class TransferViewController: UITableViewController, UIAlertViewDelegate {
     var transfer:NSDictionary?
+    var activityIndicator = UIActivityIndicatorView()
+    var progressBarButtton = UIBarButtonItem()
+    var refreshBarButton = UIBarButtonItem()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,11 +25,19 @@ class TransferViewController: UITableViewController {
     }
 
     override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+
         var transferInfoCellNib = UINib(nibName: "TransferCell", bundle: nil)
 
         tableView.registerNib(transferInfoCellNib, forCellReuseIdentifier: "TransferCell")
         tableView.registerClass(UITableViewCell.classForCoder(), forCellReuseIdentifier: "UITableViewCell")
         tableView.registerClass(KeyValueTableViewCell.classForCoder(), forCellReuseIdentifier: "KeyValueTableViewCell")
+
+
+        // Make Navigation Bar buttons
+        self.activityIndicator = UIActivityIndicatorView(frame: CGRectMake(0, 0, 20, 20))
+        self.progressBarButtton = UIBarButtonItem(customView: activityIndicator)
+        self.refreshBarButton = UIBarButtonItem(title: "Refresh", style: .Plain, target: self, action: "refresh")
     }
 
     // MARK: - Table view data source
@@ -38,6 +49,8 @@ class TransferViewController: UITableViewController {
         switch section {
             case 2:
                 return 24
+            case 1:
+                return 2
             default:
                 return 1
         }
@@ -65,12 +78,29 @@ class TransferViewController: UITableViewController {
         case 1:
             let cell = tableView.dequeueReusableCellWithIdentifier("UITableViewCell", forIndexPath: indexPath) as UITableViewCell
 
-            cell.textLabel.text = "Go to file"
-            cell.accessoryType = .DisclosureIndicator
+            if indexPath.row == 0 {
+                cell.textLabel.text = "Go to file"
+                cell.accessoryType = .DisclosureIndicator
 
-            // Disable Go to file link
-            cell.userInteractionEnabled = false
-            cell.textLabel.textColor = UIColor.grayColor()
+                // Disable Go to file link for now
+                cell.userInteractionEnabled = false
+                cell.textLabel.textColor = UIColor.grayColor()
+            } else {
+                // Disabled cancel button by default
+                cell.accessoryType = .DisclosureIndicator
+                cell.textLabel.text = "Cancel this transfer"
+                cell.userInteractionEnabled = false
+                cell.textLabel.textColor = UIColor.grayColor()
+
+                if transfer != nil {
+                    if let status = transfer!["status"]! as? String {
+                        if status == "DOWNLOADING" {
+                            cell.userInteractionEnabled = true
+                            cell.textLabel.textColor = UIColor.redColor()
+                        }
+                    }
+                }
+            }
 
             return cell
         case 2:
@@ -86,4 +116,62 @@ class TransferViewController: UITableViewController {
         }
     }
 
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+
+        if indexPath.section == 1 && indexPath.row == 1 {
+            if let status = transfer!["status"]! as? String {
+                if status == "DOWNLOADING" {
+                    let alert = UIAlertView(title: "Do you want to coancel this transfer?", message: "", delegate: self, cancelButtonTitle: "No", otherButtonTitles: "Yes")
+                    alert.show()
+                } else {
+                    // TODO clean
+                }
+            }
+
+        }
+
+    }
+
+    // MARK: - Alert view
+    func alertView(alertView: UIAlertView, clickedButtonAtIndex buttonIndex: Int) {
+        if buttonIndex == 1 { // Yes
+            if let transferId = transfer!["id"] as? NSInteger {
+                let request = HTTPTask()
+                let account = AccountStore.getAccountSync()
+                let url = "https://api.put.io/v2/transfers/cancel"
+                var params: Dictionary<String, String> = [
+                    "transfer_ids": "\(transferId)",
+                    "access_token": "\(account!.token!)"
+                ]
+
+                startProgress()
+                request.POST(url, parameters: params, success: {(response: HTTPResponse) in
+                    self.backToRoot()
+                    self.stopProgress()
+                }, failure: {(error: NSError, response: HTTPResponse?) in
+                    print(error)
+                    self.stopProgress()
+                })
+            }
+        }
+    }
+
+    func backToRoot() {
+        self.navigationController?.popToRootViewControllerAnimated(true)
+    }
+
+    func startProgress() {
+        activityIndicator.startAnimating()
+        activityIndicator.activityIndicatorViewStyle = .Gray
+        navigationItem.rightBarButtonItem = progressBarButtton
+    }
+
+    func stopProgress() {
+        navigationItem.rightBarButtonItem = refreshBarButton
+    }
+
+    func refresh () {
+
+    }
 }
