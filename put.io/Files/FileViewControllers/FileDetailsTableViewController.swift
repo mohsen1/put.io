2c5edc72153f85f3fb6ed1a895d486a10b74d805
Mohsen Azimi <msn.azi@gmail.com>
2014-12-04 20:20:28 -0800

    Add properties to FileDetailsTableViewController

--- put.io/Files/FileViewControllers/FileDetailsTableViewController.swift
+++ put.io/Files/FileViewControllers/FileDetailsTableViewController.swift
@@ -8,13 +8,29 @@
 
 import UIKit
 
+
+class DetailsTabeViewCell: UITableViewCell {
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: .Value1, reuseIdentifier: reuseIdentifier)
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
+
 class FileDetailsTableViewController: UITableViewController {
     var file:File?
+    private let dateFormatter = NSDateFormatter()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = "Details"
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Done, target: self, action: Selector("close"))
+        tableView.registerClass(DetailsTabeViewCell.classForCoder(), forCellReuseIdentifier: "DetailsTabeViewCell")
+        tableView.allowsSelection = false
+        dateFormatter.dateStyle = .MediumStyle
+        dateFormatter.timeStyle = .MediumStyle
     }
     
     func close() {
@@ -24,26 +40,48 @@ class FileDetailsTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Potentially incomplete method implementation.
-        // Return the number of sections.
-        return 0
+        return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete method implementation.
-        // Return the number of rows in the section.
-        return 0
+        return 3
     }
 
-    /*
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as UITableViewCell
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCellWithIdentifier("DetailsTabeViewCell", forIndexPath: indexPath) as UITableViewCell
+        switch indexPath.row {
+        case 0:
+            cell.textLabel?.text = "Name"
+            cell.detailTextLabel?.text = file?.name
+            break
+        case 1:
+            cell.textLabel?.text = "Accessed at"
+            cell.detailTextLabel?.text = dateFormatter.stringFromDate(file!.first_accessed_at!)
+        case 2:
+            cell.textLabel?.text = "MP4 available"
+            if let yes = file?.is_mp4_available {
+                cell.detailTextLabel?.text = file!.is_mp4_available! == 0 ? "No" : "Yes"
+            } else {
+                cell.detailTextLabel?.text = "No"
+            }
+        default:
+            break
+        }
+        
+        cell.detailTextLabel?.numberOfLines = 0
+        cell.sizeToFit()
+        
         return cell
     }
-    */
+
+    
+//    override func tableView(tableView: UITableView, estimatedHeightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+//        let cell = tableView.dequeueReusableCellWithIdentifier("DetailsTabeViewCell", forIndexPath: indexPath) as UITableViewCell
+//        let frame = cell.frame
+//        
+//    }
+//    
 
     /*
     // Override to support conditional editing of the table view.
