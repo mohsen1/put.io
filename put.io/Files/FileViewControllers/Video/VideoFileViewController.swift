6bde94bb7bb1bd42738b0f7f1edb57c72083df1a
Mohsen Azimi <msn.azi@gmail.com>
2014-12-27 13:10:18 -0800

    Play movie on click of the button

--- put.io/Files/FileViewControllers/Video/VideoFileViewController.swift
+++ put.io/Files/FileViewControllers/Video/VideoFileViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class VideoFileViewController: FileViewController {
     @IBOutlet var screenshot: UIImageView!
+    @IBOutlet var playButton: UIButton!
+    var streamUrl:NSURL?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,12 +19,33 @@ class VideoFileViewController: FileViewController {
         
         dispatch_async(dispatch_get_main_queue()){
             self.loadScreenshot()
+            self.getStreamUrl()
         }
     }
     
     func loadScreenshot() {
         if self.file != nil {
+            // TODO: cache the screenshot
             self.screenshot.image = UIImage(data: NSData(contentsOfURL: NSURL(string: self.file!.screenshot!)!)!)
+            self.playButton.hidden = false
+        }
+    }
+    
+    func getStreamUrl() {
+        if self.file != nil {
+            FileStore.getDownloadUrl(file!.id) { (url:NSURL?) in
+                self.streamUrl = url
+            }
+        }
+    }
+    
+    @IBAction func startPlaying(sender: UIButton) {
+        if streamUrl != nil {
+
+            var player = MPMoviePlayerViewController(contentURL: streamUrl!)
+            presentMoviePlayerViewControllerAnimated(player)
+            player.moviePlayer.fullscreen = true
+            player.moviePlayer.play()
         }
     }
 
